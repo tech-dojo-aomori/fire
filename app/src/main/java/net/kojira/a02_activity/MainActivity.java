7358cf27f052c410942bad1db89bca2a24c342e5
SI-NOTE-07\TECH <school3of1lock4@gmail.com>
2018-01-21 17:38:10 +0900

    01211738

--- app/src/main/java/net/kojira/a02_activity/MainActivity.java
+++ app/src/main/java/net/kojira/a02_activity/MainActivity.java
@@ -4,24 +4,28 @@ package net.kojira.a02_activity;
         import android.os.Bundle;
         import android.view.View;
         import android.widget.Button;
+        import android.widget.ImageView;
         import android.widget.TextView;
         import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     TextView text;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text=findViewById(R.id.text);
+        imageView=findViewById(R.id.imageView3);
         Button button=findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view){
                 Toast.makeText(MainActivity.this, "blue", Toast.LENGTH_SHORT).show();
                 text.setText("blue");
+                imageView.setImageResource(R.drawable.oil);
             }
         });
     }
