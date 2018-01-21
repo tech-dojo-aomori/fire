d08f2758757dd153ee1c492abf78c8e49f56d7b3
SI-NOTE-07\TECH <school3of1lock4@gmail.com>
2018-01-21 15:56:02 +0900

    プログラム

--- app/src/main/java/net/kojira/a02_activity/MainActivity.java
+++ app/src/main/java/net/kojira/a02_activity/MainActivity.java
@@ -1,13 +1,28 @@
 package net.kojira.a02_activity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text=findViewById(R.id.text);
+        Button button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "blue", Toast.LENGTH_SHORT).show();
+                text.setText("blue");
+            }
+        });
     }
 }
